c83f464a194e86262d371dfca184a6118c8c75b3
Niraj Kamdar <nirajkamdar27@pm.me>
2020-09-22 17:05:20 +0530

    Fix memcache problem in heroku
    Simulate memcache functionality in Python for demonstration purpose.

--- app/cache.py
+++ app/cache.py
@@ -1,33 +1,30 @@
-import os
 import pickle
 
 from pymemcache.client import base
 
 
 class Cache:
-    HOST = os.getenv("MEMCACHED_HOST")
-    PORT = os.getenv("MEMCACHED_PORT")
-    TTL = os.getenv("MEMCACHED_TTL")
+    """ 
+    dummy memcached for heroku since memcached extension is paid! 
+    Cons: This will only work if no. of workers is one.
+    """
 
-    def __init__(self, host="localhost", port=11211, ttl=86400):
-        self.host = self.__class__.HOST or host
-        self.port = self.__class__.PORT or port
-        self.ttl = self.__class__.TTL or ttl
-        self.client = base.Client((self.host, self.port))
+    def __init__(self):
+        self.data = {}
 
     def __getitem__(self, key):
-        value = self.client.get(key)
+        value = self.data.get(key)
         if value:
             return pickle.loads(value)
         else:
             raise KeyError
 
     def __setitem__(self, key, value):
-        self.client.set(key, pickle.dumps(value), expire=self.ttl)
+        self.data[key] = pickle.dumps(value)
 
     def __delitem__(self, key):
-        value = self.client.get(key)
+        value = self.data.get(key)
         if value:
-            self.client.delete(key)
+            del self.data[key]
         else:
             raise KeyError
